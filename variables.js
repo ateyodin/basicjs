addcd6b2e0932799a030b76466903700e567f4a2
ateyodin <52425179+ateyodin@users.noreply.github.com>
2020-06-02 20:54:03 -0400

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,25 @@
+//Las variables son la representación de algún lugar en memoria que nosotros vamos a usar para poder guardar un valor
+
+//Ejemplo 
+
+var nombre = "Andrés"; // el punto y coma nos dice que ya terminamos un comando
+
+//declarar una variable 
+
+var edad;
+
+[//inicializar](//inicializar) una variable
+
+edad = 32;
+
+// conjuto de elementos
+
+var elementos = ["computadora", "celular"];
+
+var persona = {
+
+nombre: "Andrés",
+
+edad: 30
+
+}
