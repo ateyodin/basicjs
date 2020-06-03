5dbdfe8c343c3859709b6cf3ae6ee150db3b8b37
ateyodin <52425179+ateyodin@users.noreply.github.com>
2020-06-02 21:44:55 -0400

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,23 @@
+[//Declarativas](//declarativas) 
+
+function miFuncion() [//como](//como) estamos usando una palabra reservada se declara automáticamente una función 
+
+{
+
+return 3;
+
+}// una función que inicializa un valor en la memoria y lo guarda 
+
+miFunción( );
+
+//Expresión
+
+var miFunción  = function(a,b)// una función también necesita parámetros y al momento que nosotros llamamos la función escribimos esos parámetros que nuestra función espera 
+
+{
+
+return a + b; [//que](//que) no retorne el resultado para poder llamarlo afuera de la función 
+
+}
+
+miFunción( );
